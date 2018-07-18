99c28dc86d35030bb5c9aaf661d9567e612f9154
yechs <chshu@protonmail.ch>
2018-07-19 02:51:53 +0800

    UI Polishing

--- client/app.js
+++ client/app.js
@@ -11,3 +11,22 @@ Meteor.subscribe("allAnnouncements");
 Meteor.subscribe("mentorsOnline");
 
 Meteor.subscribe("settings");
+
+moment.updateLocale('en', {
+    relativeTime : {
+        future: "%s",
+        past:   "%s 前",
+        s  : '几秒',
+        ss : '%d 秒',
+        m:  "1 分钟",
+        mm: "%d 分钟",
+        h:  "1 小时",
+        hh: "%d 小时",
+        d:  "1 天",
+        dd: "%d 天",
+        M:  "1 月",
+        MM: "%d 月",
+        y:  "1 年",
+        yy: "%d 年"
+    }
+});
